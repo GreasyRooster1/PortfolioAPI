b47d7d35049c3c93734d922dba55695785d64829
GreasyRooster <greasyRooster1@gmail.com>
2026-07-28 21:03:33 -0700

    version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(version)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[get("/version")]
+async fn version() -> impl Responder {
+    HttpResponse::Ok().body("V0.1.0")
 }
