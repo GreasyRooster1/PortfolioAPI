f0c8b505b12c4633e532f4a0c5a1d426663f3dce
GreasyRooster <greasyRooster1@gmail.com>
2026-07-29 20:27:34 -0700

    remove spam log

--- src/main.rs
+++ src/main.rs
@@ -165,7 +165,6 @@ async fn update_qcode_project_count(){
 fn calculate_user_lines(projs: &Value) -> i32 {
     let mut total:i32 = 0;
     for (key,data) in projs.as_object().unwrap() {
-        info!("{key}: {data}");
         let code = match data.get("code") {
             Some(code) => code.as_str().unwrap_or(""),
             None => continue,
