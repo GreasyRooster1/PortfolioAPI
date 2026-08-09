83c858e0326bde0dcf4be757223f58c32f7ebdd9
GreasyRooster <greasyRooster1@gmail.com>
2026-08-09 09:46:48 -0700

    cache

--- src/ip.rs
+++ src/ip.rs
@@ -1,6 +1,26 @@
 use actix_web::HttpRequest;
 use ipgeolocate::{Locator, Service};
+use tracing::{error, info};
 use tracing::log::warn;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize,Deserialize)]
+struct IpCache{
+    found:Vec<IpData>,
+}
+
+#[derive(Serialize,Deserialize)]
+struct IpData{
+    ip:String,
+    city:String,
+    region:String,
+    country:String,
+    latitude:String,
+    longitude:String,
+    isp: String,
+
+    count:u64,
+}
 
 pub(crate) async fn track_ip(req: HttpRequest){
     let client_ip = req
@@ -13,9 +33,15 @@ pub(crate) async fn track_ip(req: HttpRequest){
     };
     let service = Service::IpApi;
 
-    match Locator::get(ip, service).await {
-        Ok(ip) => println!("{} - {}, {} ({}) @ {}N {}W isp:{}", ip.ip, ip.city, ip.region, ip.country, ip.latitude, ip.longitude,ip.isp),
-        Err(error) => println!("Error: {}", error),
+    let data = match Locator::get(ip, service).await {
+        Ok(ip) => ip,
+        Err(error) => error!("Error: {}", error),
     };
+    info!("Geolocated ip: {} - {}, {} ({}) @ {}N {}W isp:{}", data.ip, data.city, data.region, data.country, data.latitude, data.longitude,data.isp);
+
     println!("{:?}", ip);
+}
+
+pub fn update_cache(data:Locator){
+
 }
